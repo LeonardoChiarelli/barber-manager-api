6df84205a20ffc38be17e4a0db976376a33e3acf
LeonardoChiarelli <devleochiarelli@gmail.com>
2026-01-31 12:02:45 -0300

    Initial Commit

---
+++ src/test/java/br/com/tenantSystem/TenantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tenantSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
